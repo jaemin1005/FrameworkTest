dcd46faf4c66f672e4aad53ffad3db0b9ca135c7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-23 17:01:48 +0900

    Feat | vite.config.ts
    vanilla-extract에 관한 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import react from '@vitejs/plugin-react-swc';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 export default defineConfig({
   server: {
     port: 3000,
@@ -10,6 +11,7 @@ export default defineConfig({
 
   plugins : [
     react(),
+    vanillaExtractPlugin(),
     resolve(),
     commonjs(),
     visualizer ({open : true})
