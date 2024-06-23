11223dbb966dea7925b03071c1592900caa55a56
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-23 16:28:37 +0900

    Feat | tsconfig.json, vite.config.ts
    React에 관한 플러그인 및 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   server: {
     port: 3000,
   },
 
   plugins : [
+    react(),
     resolve(),
     commonjs(),
     visualizer ({open : true})
