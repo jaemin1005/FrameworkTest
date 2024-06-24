789f546907649d6c76f92b116a54c625ea8757b6
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-24 20:29:30 +0900

    New | next.config.js
    reactStrictMode : strict 모드 적용
    pageExtensinon : page의 확장자 지정
    tsconfigPath : tsconfig 경로 지정

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  reactStrictMode: true,
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  typescript: {
+    tsconfigPath: "src/tsconfig.json"
+  },
+};
