a973b5a4aa3e6c7ecb39ad103eb7420ece4b7008
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-26 23:40:32 +0900

    Feat | app.module.ts
    ChatGateWay Providers 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [BookModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
