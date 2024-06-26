d2c514bfaeba435d5f532fa3c87655a2f097bf8d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-26 19:42:37 +0900

    Modify | index.tsx
    nest 서버랑 연결을 하여 데이터를 받아온다,

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,31 @@
 // src/pages/index.tsx
 import React from 'react';
 import { text } from '../styles/styles.css';
+import { GetStaticProps } from 'next';
 
+interface HomePageProps {
+  message: string;
+}
 
 // React.FC는 타입스크립트에서 React 함수형 컴포넌트를 정의하는 데 사용됩니다.
-const HomePage: React.FC = () => {
+const HomePage: React.FC<HomePageProps> = ({message}) => {
   return (
     <div>
-      {/* 이곳에 Next.js 페이지의 메인 컨텐츠를 작성합니다 */}
-      <h1 className={text}>Welcome to Next.js!</h1>
+      <h1 className={text}>{message}</h1>
     </div>
   );
 };
+
+export const getStaticProps : GetStaticProps = async () => {
+  const res = await fetch('http://localhost:3010/book');
+  const data = await res.text();  // if the response is a plain text, use res.text()
+
+  return {
+    props: {
+      message: data,
+    },
+  };
+}
 
 // HomePage 컴포넌트를 기본 익스포트로 설정하여 이 파일을 페이지로 사용합니다.
 export default HomePage;
